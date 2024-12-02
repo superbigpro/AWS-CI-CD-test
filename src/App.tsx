4fdc1e98f2ff936539e8d7da6831f01ff0a99f7c
leeyeonwoo <rexrex1105@gmail.com>
2024-12-02 22:53:16 +0900

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>CI/CD 테스트 페이지입니다.</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  );
+}
+
+export default App;
